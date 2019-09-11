0842e16d5eca66cc9285821627156aa2ff9bdc50
Guy Watson <guy.w@me.com>
2019-09-10 23:25:10 -0600

    Small text example, including using a list of ids

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,8 @@ use std::net::TcpStream;
 use std::io::{Write, Read};
 use std::collections::HashMap;
 use glium::Surface;
+use conrod_core::{Color, Colorable, text, Widget};
+use conrod_core::position::Dimension;
 
 mod support;
 
@@ -230,92 +232,58 @@ fn set_widgets(ref mut ui: conrod_core::UiCell, ids: &mut Ids) {
     use conrod_core::{color, widget, Colorable, Labelable, Positionable, Sizeable, Widget};
 
     // Construct our main `Canvas` tree.
-    widget::Canvas::new().flow_down(&[
-        (ids.header, widget::Canvas::new().color(color::BLUE).pad_bottom(20.0)),
-        (ids.body, widget::Canvas::new().length(300.0).flow_right(&[
-            (ids.left_column, widget::Canvas::new().color(color::LIGHT_ORANGE).pad(20.0)),
-            (ids.middle_column, widget::Canvas::new().color(color::ORANGE)),
-            (ids.right_column, widget::Canvas::new().color(color::DARK_ORANGE).pad(20.0)),
-        ])),
-        (ids.footer, widget::Canvas::new().color(color::BLUE).scroll_kids_vertically()),
-    ]).set(ids.master, ui);
+    widget::Canvas::new().color(color::WHITE).set(ids.master, ui);
 
-    // A scrollbar for the `FOOTER` canvas.
-    widget::Scrollbar::y_axis(ids.footer).auto_hide(true).set(ids.footer_scrollbar, ui);
+    widget::Rectangle::outline([10., 40.])
+        .color(color::TRANSPARENT)
+        .x(0.0)
+        .y(0.0)
+        .set(ids.rectangle, ui);
 
-    // Now we'll make a couple floating `Canvas`ses.
-    let floating = widget::Canvas::new().floating(true).w_h(110.0, 150.0).label_color(color::WHITE);
-    floating.middle_of(ids.left_column).title_bar("Blue").color(color::BLUE).set(ids.floating_a, ui);
-    floating.middle_of(ids.right_column).title_bar("Orange").color(color::LIGHT_ORANGE).set(ids.floating_b, ui);
+//    println!("Rect of {:?}", ui.rect_of(ids.master));
 
-    // Here we make some canvas `Tabs` in the middle column.
-    widget::Tabs::new(&[(ids.tab_foo, "FOO"), (ids.tab_bar, "BAR"), (ids.tab_baz, "BAZ")])
-        .wh_of(ids.middle_column)
-        .color(color::BLUE)
-        .label_color(color::WHITE)
-        .middle_of(ids.middle_column)
-        .set(ids.tabs, ui);
+    //set the amount of text
+    ids.text.resize(4, &mut ui.widget_id_generator());
+    widget::Text::new("G")
+        .color(color::BLACK)
+        .x_y(-200.0, 200.0)
+        .set(ids.text[0], ui);
+    widget::Text::new("u")
+        .color(color::BLACK)
+        .x_y(-190.0, 200.0)
+        .set(ids.text[1], ui);
+    widget::Text::new("y")
+        .color(color::BLACK)
+        .x_y(-180.0, 200.0)
+        .set(ids.text[2], ui);
+    widget::Text::new("Watson")
+        .color(color::BLACK)
+        .x_y(-140.0, 200.0)
+        .set(ids.text[3], ui);
 
-    widget::Text::new("Fancy Title")
-        .color(color::LIGHT_ORANGE)
-        .font_size(48)
-        .middle_of(ids.header)
-        .set(ids.title, ui);
-    widget::Text::new("Subtitle")
-        .color(color::BLUE.complement())
-        .mid_bottom_of(ids.header)
-        .set(ids.subtitle, ui);
+    //todo: make a helper method that uses `ui.rect_of(ids.text[3])` in order to
+    //  compute the absolute position within the window. The current behavior is
+    //  that (0,0) is the center point of the widget, not the top left.
+    //  I want absolute -> equivalent relative
 
-    widget::Text::new("Top Left")
-        .color(color::LIGHT_ORANGE.complement())
-        .top_left_of(ids.left_column)
-        .set(ids.top_left, ui);
 
-    widget::Text::new("Bottom Right")
-        .color(color::DARK_ORANGE.complement())
-        .bottom_right_of(ids.right_column)
-        .set(ids.bottom_right, ui);
 
-    fn text(text: widget::Text) -> widget::Text { text.color(color::WHITE).font_size(36) }
-    text(widget::Text::new("Foo!")).middle_of(ids.tab_foo).set(ids.foo_label, ui);
-    text(widget::Text::new("Bar!")).middle_of(ids.tab_bar).set(ids.bar_label, ui);
-    text(widget::Text::new("BAZ!")).middle_of(ids.tab_baz).set(ids.baz_label, ui);
 
-    let footer_wh = ui.wh_of(ids.footer).unwrap();
-    let mut elements = widget::Matrix::new(COLS, ROWS)
-        .w_h(footer_wh[0], footer_wh[1] * 2.0)
-        .mid_top_of(ids.footer)
-        .set(ids.button_matrix, ui);
-    while let Some(elem) = elements.next(ui) {
-        let (r, c) = (elem.row, elem.col);
-        let n = c + r * c;
-        let luminance = n as f32 / (COLS * ROWS) as f32;
-        let button = widget::Button::new().color(color::BLUE.with_luminance(luminance));
-        for _click in elem.set(button, ui) {
-            println!("Hey! {:?}", (r, c));
-        }
-    }
 
-    let button = widget::Button::new().color(color::RED).w_h(30.0, 30.0);
-    for _click in button.clone().middle_of(ids.floating_a).set(ids.bing, ui) {
-        println!("Bing!");
-    }
-    for _click in button.middle_of(ids.floating_b).set(ids.bong, ui) {
-        println!("Bong!");
-    }
+
+
+
 }
-
-// Button matrix dimensions.
-const ROWS: usize = 10;
-const COLS: usize = 24;
 
 // Generate a unique `WidgetId` for each widget.
 // Generates the boilerplate for all the `button: conrod::widget::Id, ...`
 widget_ids! {
     struct Ids {
+        master,
         rectangle,
         oval,
-        text,
+        text[],
+        text2,
 //        master,
 //        header,
 //        body,
